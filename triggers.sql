a74f8d0734a6622d1b3042936762d2b6ba84375e
Aniket <aniketgupta3001@gmail.com>
2024-04-19 03:39:19 +0530

    Made triggers.sql and wrote the description of my triggers

---
+++ triggers.sql
@@ -0,0 +1,9 @@
+use database DailyHarbour;
+
+-- 1) Empty the cart for the user once they place the order, i.e a record has been inserted into the order_details table
+
+
+-- 2) Deplete the total stock once the user adds respective items to cart
+
+
+-- 3) Update the total stock when user gets timed out and the items are removed from the cart
